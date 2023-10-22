67a907848acfb43d81251964a6041d582753a053
sandisile-12t <sandisileprecious0@gmail.com>
2023-10-22 22:31:47 +0200

    CRUD operation
    Administrator CRUD Application
    
    An application that allows administrators to perform CRUD operations on user data.
    
    - Create- Add new user records to the system.
    - Read- Retrieve and view user data.
    - Update- Modify existing user information.
    - Delete-  Remove user records from the database.

--- app/routers/companies.py
+++ app/routers/companies.py
@@ -10,16 +10,81 @@ router = APIRouter(prefix="/companies", tags=["Companies"], include_in_schema=Fa
 
 templates = Jinja2Templates(directory="templates")
 
+@router.get("/")
+async def home(request: Request, db: Session = Depends(get_db)):
+    companies = db.query(Company).order_by(Company.id.asc()).all()
+    return templates.TemplateResponse("index.html", {"request": request, "companies": companies})
 
-@router.get("/", response_class=HTMLResponse, name="companies.index")
-async def create(request: Request):
-    companies = [{"id": 1, "name": "SNAPI Inc.", "registration_number": "234-5673-GH", }]
-    applications = [{"id": 1, "name": "MultiPlatform Flutter", "created_at": "2023-06-11", }]
+@router.post("/add")
+async def add(
+    request: Request,
+    name: str = Form(...),
+    registration_number: int = Form(...),
+    main_contact_number: int = Form(...),
+    secondary_contact_number: int = Form(...),
+    user_id: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    try:
+        current_datetime = datetime.now()
+        company = Company(
+            name=name,
+            registration_number=registration_number,
+            main_contact_number=main_contact_number,
+            secondary_contact_number=secondary_contact_number,
+            created_at=current_datetime,
+            updated_at=current_datetime,
+            user_id=user_id
+        )
+        db.add(company)
+        db.commit()
+        return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
 
-    return templates.TemplateResponse("pages/companies/index.html",
-                                      {"request": request, "applications": applications, "companies": companies, })
+@router.get("/createnew")
+async def createnew(request: Request, user_id: int = Cookie(None), db: Session = Depends(get_db)):
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="User not authenticated")
+    return templates.TemplateResponse("createnew.html", {"request": request, "user_id": user_id})
 
+@router.get("/edit/{company_id}")
+async def edit_company(request: Request, company_id: int, db: Session = Depends(get_db)):
+    company = db.query(Company).filter(Company.id == company_id).first()
+    return templates.TemplateResponse("edit.html", {"request": request, "company": company})
 
-@router.post("/create", response_class=HTMLResponse)
-async def store_apikey(request: Request):
-    return templates.TemplateResponse("pages/companies/create.html", {"request": request, "message": "Success"})
+@router.post("/update/{company_id}")
+async def update(
+    request: Request,
+    company_id: int,
+    name: str = Form(...),
+    registration_number: int = Form(...),
+    main_contact_number: int = Form(...),
+    secondary_contact_number: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    company = db.query(Company).filter(Company.id == company_id).first()
+    if company:
+        company.name = name
+        company.registration_number = registration_number
+        company.main_contact_number = main_contact_number
+        company.secondary_contact_number = secondary_contact_number
+        db.commit()
+        return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        raise HTTPException(status_code=404, detail="Company not found")
+
+@router.get("/delete/{company_id}")
+async def delete(
+    request: Request,
+    company_id: int,
+    db: Session = Depends(get_db)
+):
+    company = db.query(Company).filter(Company.id == company_id).first()
+    db.delete(company)
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
